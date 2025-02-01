b24e1b5a6b94932985e139f0fb35a0b9a79e643d
Thomas <tlemaire1610@gmail.com>
2025-02-01 17:54:33 +0100

    ✨ add main title

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
+import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <h1 className={styles.title}>La Quinta</h1>
+        <div className={styles.mainTitleContainer}>
+          <h1 className={styles.mainTitle}>La Quinta</h1>
+          <h2 className={styles.mainSubtitle}>Traiteur portugais</h2>
+          <Image src="/roasted-chicken.jpg" alt="chicken" width={100} height={100}></Image>
+        </div>
       </main>
       <footer className={styles.footer}>
         <p>footer</p>
