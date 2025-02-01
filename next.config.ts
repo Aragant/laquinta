6efa4cb8b09d06718eeeb5bf10134accea8e0c00
Thomas <tlemaire1610@gmail.com>
2025-02-01 16:54:19 +0100

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,9 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
-  assetPrefix: isProd ? "/aragant.github.io/" : "",
-  basePath: isProd ? "/aragant.github.io" : "",
-  images: {
-    unoptimized: true, // Désactive l'optimisation Next.js des images pour GitHub Pages
-  },
+  /* config options here */
+  basePath: '/laquinta'
+  
 };
 
 export default nextConfig;
