d2b9bda735fe51c8b8305a5dd3e3eaa6d6887042
Thomas <tlemaire1610@gmail.com>
2025-02-01 16:27:56 +0100

    🔧 update next.config to unoptimized when we are on prod to display image for my static web site

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  assetPrefix: isProd ? "/aragant.github.io/" : "",
+  basePath: isProd ? "/aragant.github.io" : "",
+  images: {
+    unoptimized: true, // Désactive l'optimisation Next.js des images pour GitHub Pages
+  },
 };
 
 export default nextConfig;
