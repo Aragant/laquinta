a80590d55835563230d682a5bd7003625c5f5f6d
Thomas <tlemaire1610@gmail.com>
2025-02-01 18:18:51 +0100

    🐛 test with a base path

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-// import Image from "next/image";
+import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
@@ -8,7 +8,7 @@ export default function Home() {
         <div className={styles.mainTitleContainer}>
           <h1 className={styles.mainTitle}>La Quinta</h1>
           <h2 className={styles.mainSubtitle}>Traiteur portugais</h2>
-          <img src="./roasted-chicken.png" alt="chicken" width={400} height={400}></img>
+          <Image src="/roasted-chicken.png" alt="chicken" width={400} height={400}></Image>
         </div>
       </main>
       <footer className={styles.footer}>
