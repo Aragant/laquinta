1825935a8a4eee13b80faa7f8f72660cd0f613d7
Thomas <tlemaire1610@gmail.com>
2025-03-05 22:23:05 +0100

    ✨ Finish the bases

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import styles from "./page.module.css";
 import Image from "next/image";
 
@@ -42,16 +44,16 @@ export default function Home() {
             ></Image>
           </div>
 
-          <div className={styles.pastel}>
-            <Image
-              className={`${styles.pastelImage} ${styles.imageFramed}`}
-              src={"/pastel.jpg"}
-              alt={"Picture for decorate the page"}
-              fill={true}
-            ></Image>
-          </div>
-
           <div className={styles.ticketHoraire}>
+            <div className={styles.pastel}>
+              <Image
+                className={`${styles.pastelImage} ${styles.imageFramed}`}
+                src={"/pastel.jpg"}
+                alt={"Picture for decorate the page"}
+                fill={true}
+              ></Image>
+            </div>
+
             <h2 className={styles.titleHoraire}>NOS HORAIRES</h2>
             <p className={styles.textHoraire}>
               Períodos específicos do dia determinados para a realização
@@ -126,9 +128,140 @@ export default function Home() {
         </div>
 
         <div className={`${styles.vinoPresDiv} `}>
-            <div className={`${styles.imageFramed} ${styles.vinoPresImage}`}>
+          <div
+            className={`${styles.imageFramed} ${styles.vinoPresImage}`}
+          ></div>
+        </div>
 
+        <div className={styles.addressDiv}>
+          <div className={styles.cardAddress}>
+            <div className={styles.portugalDiv}>
+              <Image
+                src={"/Portugal.png"}
+                alt={"Picture for decorate the page"}
+                fill={true}
+              ></Image>
             </div>
+
+            <div className={styles.textAddressDiv}>
+              <h3 className={styles.titleAddress}>Nous rendre visite</h3>
+              <p className={styles.subtitleAddress}>
+                Toutes les saveurs du Portugal
+              </p>
+
+              <p className={styles.roundedTitleAddress}>Notre adresse</p>
+              <p className={styles.textStreetAddress}>
+                20 rue maurice
+                <br />
+                berteaux
+              </p>
+
+              <p className={styles.textCityAddress}>
+                78700 Conflans-
+                <br />
+                Sainte-Honorine
+              </p>
+
+              <hr className={styles.lineAddress} />
+
+              <p className={styles.roudedEndText}>Mot de la fin</p>
+
+              <p className={styles.textSignAddress}>
+                Découvrez les saveurs
+                <br />
+                du Portugal dans une
+                <br />
+                ambiance chaleureuse.
+                <br />À bientôt chez nous !
+              </p>
+
+              <p className={styles.endTextAddress}>
+                20 rue Maurice Berteaux, 78700 Conflans-Sainte-Honorine
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.footerDiv}>
+          <div className={styles.infoFooterDiv}>
+            <div className={styles.textFooterDiv}>
+              <h3 className={styles.titleFooter}>Obrigado !</h3>
+              <p className={styles.textFooter}>
+                Spécialisé dans la nourriture
+                <br />
+                portugaise, n’hésitez pas à nous
+                <br />
+                contacter si vous avez
+                <br />
+                des demandes particulières.
+              </p>
+            </div>
+
+            <div className={styles.contactFooter}>
+              <p className={styles.textContactFooter}>
+                laquinta78@outlook.com
+                <br />
+                +33 7 72 16 94 00
+                <br />
+                +33 1 30 65 35 49
+              </p>
+            </div>
+
+            <table className={styles.socialFooter}>
+              <tbody>
+                <tr>
+                  <td
+                    scope="row"
+                    className={styles.textSocialFooter}
+                    id={styles.firstCellSocialFooter}
+                  >
+                    2025 ©LAQUINTA
+                  </td>
+                  <td className={styles.textSocialFooter}>INSTAGRAM</td>
+                  <td className={styles.textSocialFooter}>FACEBOOK</td>
+                  <td
+                    className={styles.textSocialFooter}
+                    id={styles.lastCellSocialFooter}
+                  >
+                    CREDITS
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+
+          <div className={styles.imageFooterDiv}>
+            <div className={styles.discountTicketFooter}>
+              <div className={styles.titleDiscountFooterDiv}>
+                <h4 className={styles.titleDiscountFooter}>À bientôt</h4>
+                <p className={styles.subtitleDiscountFooter}>Até breve</p>
+              </div>
+              <div className={styles.circleDiscountDiv}>
+                <p className={styles.circleDiscount}>01</p>
+                <p className={styles.circleDiscount}>02</p>
+                <p className={styles.circleDiscount}>03</p>
+                <p className={styles.circleDiscount}>04</p>
+                <p className={styles.circleDiscount}>05</p>
+                <p className={styles.circleDiscount}>06</p>
+                <p className={styles.circleDiscount}>07</p>
+                <p className={styles.circleDiscount}>08</p>
+                <div className={styles.circleDiscount} id={styles.redChickenDiv}>
+                  <div className={styles.redChickenDiscount}>
+                    <Image
+                    
+                    src={"/red-chicken.svg"}
+                    alt="Picture for decorate the page"
+                    fill={true}
+                    >
+
+                    </Image>
+                  </div>
+                </div>
+              </div>
+
+              <p className={styles.textEndDiscountFooter}>@la-quinta</p>
+            </div>
+          </div>
         </div>
       </main>
     </div>
